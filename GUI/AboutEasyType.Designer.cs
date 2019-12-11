801f73be04f531c870691bfd676e82f59274ad28
nnnghia98 <39165720+nnnghia98@users.noreply.github.com>
2019-12-11 16:09:27 +0700

    Edit about form

--- GUI/AboutEasyType.Designer.cs
+++ GUI/AboutEasyType.Designer.cs
@@ -32,16 +32,16 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // abtEasyType
             // 
             this.abtEasyType.AutoSize = true;
-            this.abtEasyType.Location = new System.Drawing.Point(112, 9);
+            this.abtEasyType.Location = new System.Drawing.Point(149, 11);
+            this.abtEasyType.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.abtEasyType.Name = "abtEasyType";
-            this.abtEasyType.Size = new System.Drawing.Size(85, 13);
+            this.abtEasyType.Size = new System.Drawing.Size(112, 17);
             this.abtEasyType.TabIndex = 0;
             this.abtEasyType.Text = "About EasyType";
             this.abtEasyType.Click += new System.EventHandler(this.abtEasyType_Click);
@@ -49,62 +49,57 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(112, 22);
+            this.label1.Location = new System.Drawing.Point(149, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.Size = new System.Drawing.Size(116, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Version: beta 0.1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Location = new System.Drawing.Point(16, 75);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.Size = new System.Drawing.Size(61, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Authors:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(112, 61);
+            this.label3.Location = new System.Drawing.Point(149, 92);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(150, 13);
+            this.label3.Size = new System.Drawing.Size(196, 17);
             this.label3.TabIndex = 3;
-            this.label3.Text = "Pham Quang Vinh - 16521446";
+            this.label3.Text = "Duong Thanh Tin - 16521241";
             this.label3.Click += new System.EventHandler(this.label3_Click);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(112, 74);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(165, 13);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Nguyen Dang Quang - 16521560";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(112, 87);
+            this.label5.Location = new System.Drawing.Point(149, 75);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(161, 13);
+            this.label5.Size = new System.Drawing.Size(212, 17);
             this.label5.TabIndex = 5;
             this.label5.Text = "Nguyen Ngoc Nghia - 16521585";
             // 
             // AboutEasyType
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(328, 125);
+            this.ClientSize = new System.Drawing.Size(437, 154);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.abtEasyType);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AboutEasyType";
@@ -123,7 +118,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
     }
 }
